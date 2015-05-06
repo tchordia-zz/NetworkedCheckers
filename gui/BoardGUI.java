0d506598045050a7e82383183956151e226a27d0
screwz38 <stevenz981@gmail.com>
2015-05-06 10:07:57 -0700

    gui package

---
+++ gui/BoardGUI.java
@@ -0,0 +1,14 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class BoardGUI extends JFrame implements ActionListener
+{
+    public void actionPerformed( ActionEvent arg0 )
+    {
+        // TODO Auto-generated method stub
+        
+    }
+}
