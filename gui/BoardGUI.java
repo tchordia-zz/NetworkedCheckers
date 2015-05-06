6be7fa5645697de878586ecc1b121300403fa243
screwz38 <stevenz981@gmail.com>
2015-05-06 10:51:07 -0700

    checkers world

--- gui/BoardGUI.java
+++ gui/BoardGUI.java
@@ -2,10 +2,21 @@ package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+
+import Model.CheckerBoard;
 
 public class BoardGUI extends JFrame implements ActionListener
 {
+    private CheckerBoard game;
+    
+    public BoardGUI()
+    {
+        this.game = game;
+    }
+    
+    
     public void actionPerformed( ActionEvent arg0 )
     {
         // TODO Auto-generated method stub
