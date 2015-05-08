14e0e88207938a590b3af13bd16f53165c66a6b8
screwz38 <stevenz981@gmail.com>
2015-05-08 09:07:57 -0700

    piece information for the display of the piece on the board code

---
+++ gui/Piece.java
@@ -0,0 +1,44 @@
+package gui;
+
+import java.awt.Color;
+
+
+public class Piece
+{
+    private Color c;
+
+
+    /**
+     * Constructs a piece.
+     * 
+     * @param color
+     *            the color of the piece
+     */
+    public Piece( Color color )
+    {
+        c = color;
+    }
+
+
+    /**
+     * Gets the color of the piece.
+     * 
+     * @return the color of the piece
+     */
+    public Color getColor()
+    {
+        return c;
+    }
+
+
+    /**
+     * Sets the color of the piece.
+     * 
+     * @param newColor
+     *            the new color for the piece
+     */
+    public void setColor( Color newColor )
+    {
+        c = newColor;
+    }
+}
