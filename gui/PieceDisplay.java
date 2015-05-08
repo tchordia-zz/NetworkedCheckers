7b83ab8a7361919e10aef1b3c99e3b4724c9d63b
screwz38 <stevenz981@gmail.com>
2015-05-08 09:11:02 -0700

    piece3 display on game

---
+++ gui/PieceDisplay.java
@@ -0,0 +1,26 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+
+public class PieceDisplay
+{
+
+    private Shape body;
+
+    public PieceDisplay()
+    {
+
+    }
+
+    public void draw(Object obj, Component comp, Graphics2D g2)
+    {
+
+    }
+}
