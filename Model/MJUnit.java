77dad7ba1d10cc0d638af90605b10a7ce1104f8c
tchordia619 <tchordia619@LHS-PRLAB-23.lhs.win.fuhsd.org>
2015-05-18 08:48:28 -0700

    Add a junit test

---
+++ Model/MJUnit.java
@@ -0,0 +1,163 @@
+package Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *  
+ *
+ *  @author  Tanmay Chordia
+ *  @version May 18, 2015
+ *  @author  Period: 2
+ *  @author  Assignment: APCSFinal
+ *
+ *  @author  Sources: TODO
+ */
+public class MJUnit
+{
+
+    /**
+     * TODO Write your method description here.
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+    }
+
+
+    /**
+     * TODO Write your method description here.
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+    }
+
+
+    /**
+     * TODO Write your method description here.
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+
+    /**
+     * TODO Write your method description here.
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#CheckerBoard(Model.Game)}.
+     */
+    @Test
+    public void testCheckerBoard()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#getNumRed()}.
+     */
+    @Test
+    public void testGetNumRed()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#getNumBlack()}.
+     */
+    @Test
+    public void testGetNumBlack()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#isGameOver()}.
+     */
+    @Test
+    public void testIsGameOver()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#isLegal(Model.Move)}.
+     */
+    @Test
+    public void testIsLegal()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#doMove(Model.Move)}.
+     */
+    @Test
+    public void testDoMove()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#hasJumps(int, int)}.
+     */
+    @Test
+    public void testHasJumps()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#isRedTurn()}.
+     */
+    @Test
+    public void testIsRedTurn()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#inCompoundMove()}.
+     */
+    @Test
+    public void testInCompoundMove()
+    {
+        fail( "Not yet implemented" );
+    }
+
+
+    /**
+     * Test method for {@link Model.CheckerBoard#toString()}.
+     */
+    @Test
+    public void testToString()
+    {
+        fail( "Not yet implemented" );
+    }
+
+}
