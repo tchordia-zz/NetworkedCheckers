39926828341b279c64b3c3473cbf8d4acd986d53
tchordia619 <tchordia619@LHS-PRLAB-23.lhs.win.fuhsd.org>
2015-05-19 09:15:51 -0700

    update junit

--- Model/MJUnit.java
+++ Model/MJUnit.java
@@ -6,10 +6,6 @@ import static org.junit.Assert.fail;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 
